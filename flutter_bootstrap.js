13a241bbbec7638eac7dae83695bbb8163decaa1
duhfadel <duhpicheth@gmail.com>
2026-08-21 16:32:32 +0200

    Session in the app bar, no prize-type field, and the CORS origin fixed

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"0cd610717bde95fd88343c64f81c11ba4e5c00
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3689405193" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2399509089" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
