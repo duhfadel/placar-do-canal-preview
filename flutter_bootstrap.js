ff604dfdf34d2a0fdfed6bb492c5c68d6c1ec851
Eduardo <duhpicheth@gmail.com>
2026-08-25 21:42:26 +0200

    Servir na raiz do domínio magraotv.net.br

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"0cd610717bde95fd88343c64f81c11ba4e5c00
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "836300204" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2639297333" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
